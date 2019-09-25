f090db5536f0a4ca02495f09c9bc56b44156a657
theoliao <theoliao1998@gmail.com>
2019-09-25 02:02:02 +0000

    Building and Loading UNESCO World Heritage Sites

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
+    'django_extensions', # Add
+    # 'unesco.apps.UnescoConfig',  # Add
 ]
 
 MIDDLEWARE = [
